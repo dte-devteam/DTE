d4d9be42d74de4b21a68ebec8ca29383d2faa620
Pasha-2033 <pablo.sokolik@gmail.com>
2025-07-02 22:49:58 +0300

    exception noexcept

--- Utility/exceptions/exception.cpp
+++ Utility/exceptions/exception.cpp
@@ -2,10 +2,10 @@
 using namespace dte_utils;
 exception::exception(size_t code) noexcept : _error_code(code), _what(nullptr) {}
 exception::exception(const exception& other) noexcept : _error_code(other._error_code), _what(other._what) {}
-const char* exception::what() const {
+const char* exception::what() const noexcept {
 	return _what ? _what : "Unknown error";
 }
-size_t exception::error_code() const {
+size_t exception::error_code() const noexcept {
 	return _error_code;
 }
 exception& exception::operator=(const exception& other) noexcept {
